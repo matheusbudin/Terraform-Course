e64538f13d334df59dd7f917cc96632def77a4d5
matheusbudin <matheusbudin@hotmail.com>
2023-02-09 23:13:22 -0300

    criacao de storage account da azure

---
+++ storage-account.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "first_resource_group" {
+  name     = ""
+  location = ""
+
+  tags = ""
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = ""
+  resource_group_name      = ""
+  location                 = ""
+  account_tier             = ""
+  account_replication_type = ""
+
+  tags = ""
+
+}
+
+resource "azurerm_storage_container" "first_container" {
+  name                 = ""
+  storage_account_name = ""
+  #container_access_type = "private" -> opcional
+}
