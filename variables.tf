69ab06c139de8cef86771f5a556f3b6b9927fb38
matheusbudin <matheusbudin@hotmail.com>
2023-02-13 20:49:08 -0300

    primeiras variaveis do storage-account

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
 variable "location" {
-  description = "Variavel que indica a regiao onde os recursos vao ser criados" #paode deixar vazio o bloco e indicar no cli por bash$ export location = 
+  description = "Variavel que indica a regiao onde os recursos vao ser criados"  #paode deixar vazio o bloco e indicar no cli por bash$ export location = 
+  type = string
+  default = "Brazil South"
 }   
+
+variable "account_tier" {
+    description = "Tier da Storage Account na Azure"
+    type = string
+    default = "Standard"
+  
+}
